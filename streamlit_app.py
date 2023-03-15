10c676e0aa8c357854676dce5f72b5722541f3b9
Matthew Stubenberg <matthewstubenberg@gmail.com>
2023-03-15 11:49:20 -1000

    new requirements text

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,11 @@ of Caroline County, a grand jury issued an indictment charging the Lovings with 
 After their convictions, the Lovings took up residence in the District of Columbia. On November 6, 1963, they filed a motion in the state trial court to vacate the judgment and set aside the sentence on the ground that the statutes which they had violated were repugnant to the Fourteenth Amendment. The motion not having been decided by October 28, 1964, the Lovings instituted a class action in the United States District Court for the Eastern District of Virginia requesting that a three-judge court be convened to declare the Virginia anti-miscegenation statutes unconstitutional and to enjoin state officials from enforcing their convictions. On January 22, 1965, the state trial judge denied the motion to vacate the sentences, and the Lovings perfected an appeal to the Supreme Court of Appeals of Virginia. On February 11, 1965, the three-judge District Court continued the case to allow the Lovings to present their constitutional claims to the highest state court.
 The Supreme Court of Appeals upheld the constitutionality of the anti-miscegenation statutes and, after
 Page 388 U. S. 4'''
-
+st.title("Cloud Generator")
 
 wc = WordCloud()
 wc.generate(text)
 
 plt.axis("off")
 plt.imshow(wc, interpolation="bilinear")
-plt.show()
+st.pyplot(plt)
